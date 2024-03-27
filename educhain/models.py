b7b2b47a55342bd56036ea86a7210b92214dd0f6
Satvik <satvik.pss@gmail.com>
2024-03-27 20:42:00 +0530

    modified MCQList

--- educhain/models.py
+++ educhain/models.py
@@ -2,18 +2,22 @@ from typing import List
 from langchain_core.pydantic_v1 import BaseModel
 
 class MCQ(BaseModel):
+    """A class representing a multiple choice question."""
     question: str
     options: List[str]
     correct_answer: str
 
-    def __str__(self):
-        options_str = "\n".join(f"  - {option}" for option in self.options)
-        return f"Question: {self.question}\nOptions:\n{options_str}\nCorrect Answer: {self.correct_answer}"
+    # def __str__(self):
+    #     options_str = "\n".join(f"  - {option}" for option in self.options)
+    #     return f"Question: {self.question}\nOptions:\n{options_str}\nCorrect Answer: {self.correct_answer}"
 
-class MCQList(BaseModel):
-    MCQList: List[MCQ]
 
-    def __str__(self):
-        mcq_str = "\n\n".join(str(mcq) for mcq in self.MCQList)
-        return f"MCQ List:\n\n{mcq_str}"
+    
+class MCQList(List[MCQ]):
+    """A class representing a list of multiple choice questions."""
+    pass
+    # def __str__(self):
+    #     mcq_str = "\n\n".join(str(mcq) for mcq in self)
+    #     return f"MCQs:\n\n{mcq_str}"
+
 
