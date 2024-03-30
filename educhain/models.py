bdcb3ff8eb888cff34729c842c3ded172f6d0a1e
Satvik <satvik.pss@gmail.com>
2024-03-31 02:09:56 +0530

    added show

--- educhain/models.py
+++ educhain/models.py
@@ -7,15 +7,15 @@ class MCQ(BaseModel):
     options: List[str]
     correct_answer: str
 
-    # def __str__(self):
-    #     options_str = "\n".join(f"  - {option}" for option in self.options)
-    #     return f"Question: {self.question}\nOptions:\n{options_str}\nCorrect Answer: {self.correct_answer}"
+    def show(self):
+        options_str = "\n".join(f"  - {option}" for option in self.options)
+        return f"Question: {self.question}\nOptions:\n{options_str}\nCorrect Answer: {self.correct_answer}"
 
 
 
 class MCQList(BaseModel):
     questions: List[MCQ]
 
-    # def __str__(self):
-    #     mcq_str = "\n\n".join(str(mcq) for mcq in self)
-    #     return f"MCQs:\n\n{mcq_str}"
+    def show(self):
+        mcq_str = "\n\n".join(str(mcq) for mcq in self)
+        return f"MCQs:\n\n{mcq_str}"
