acaccc5cfc17e7b5fd32096206448b198c4dc13c
Wasif Nadeem <wasif1607@gmail.com>
2024-04-02 04:18:05 +0530

    final

--- educhain/qna_engine.py
+++ educhain/qna_engine.py
@@ -4,10 +4,7 @@ from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.output_parsers import PydanticOutputParser
 from .models import MCQList ###
-from dotenv import load_dotenv
 
-load_dotenv()
-    
 def generate_mcq(topic, level, num = 1, llm = None):
 
     parser = PydanticOutputParser(pydantic_object=MCQList)
@@ -44,14 +41,3 @@ def generate_mcq(topic, level, num = 1, llm = None):
     structured_output = parser.parse(results)
         
     return structured_output
-
-# Generate a multiple-choice question
-mcq = generate_mcq(topic="Python", level="Advanced", num=10)
-print(mcq)
-
-# Save the MCQ to a CSV file
-to_csv(mcq, "mcq7.csv")
-# Save the MCQ to a PDF file
-to_pdf(mcq, "mcq7.pdf", heading="Python MCQ", subheading="Advanced Level - (10 Questions)")
-# Save the MCQ to a JSON file
-to_json(mcq, "mcq7.json")
