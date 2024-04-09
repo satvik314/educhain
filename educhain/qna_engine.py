965bfde5d446d784759783f34ebf9eb633a77085
Wasif Nadeem <wasif1607@gmail.com>
2024-04-09 17:37:48 +0530

    better approach for custom template to avoid errors

--- educhain/qna_engine.py
+++ educhain/qna_engine.py
@@ -1,4 +1,3 @@
-import json
 from .utils import to_csv, to_json, to_pdf  ###
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
@@ -6,26 +5,24 @@ from langchain.chains import LLMChain
 from langchain.output_parsers import PydanticOutputParser
 from .models import MCQList ###
 
-
-
-def generate_mcq(topic, level, num=1, llm=None, prompt_template=None, **kwargs):
+def generate_mcq(topic, level, num=1, custom_template="", llm=None, **kwargs):
     parser = PydanticOutputParser(pydantic_object=MCQList)
     format_instructions = parser.get_format_instructions()
 
-    if prompt_template is None:
-        prompt_template = """
-        Generate {num} multiple-choice question (MCQ) based on the given topic and level.
-        provide the question, four answer options, and the correct answer.
+    prompt_template = """
+    Generate {num} multiple-choice question (MCQ) based on the given topic and level.
+    provide the question, four answer options, and the correct answer.
 
-        Topic: {topic}
-        Level: {level}
-        """
-
-    # Append the JSON format instruction line to the custom prompt template
-    prompt_template += "\nThe response should be in JSON format. \n {format_instructions}"
+    Topic: {topic}
+    Level: {level}
+    
+    {custom_template}
+    
+    The response should be in JSON format. \n {format_instructions}
+    """
 
     MCQ_prompt = PromptTemplate(
-        input_variables=["num", "topic", "level"],
+        input_variables=["num", "topic", "level", "custom_template"],
         template=prompt_template,
         partial_variables={"format_instructions": format_instructions}
     )
@@ -38,7 +35,7 @@ def generate_mcq(topic, level, num=1, llm=None, prompt_template=None, **kwargs):
     MCQ_chain = LLMChain(llm=llm, prompt=MCQ_prompt)
 
     results = MCQ_chain.invoke(
-        {"num": num, "topic": topic, "level": level, **kwargs},
+        {"num": num, "topic": topic, "level": level, "custom_template": custom_template, **kwargs},
         return_only_outputs=True
     )
 
@@ -46,4 +43,3 @@ def generate_mcq(topic, level, num=1, llm=None, prompt_template=None, **kwargs):
     structured_output = parser.parse(results)
 
     return structured_output
-
