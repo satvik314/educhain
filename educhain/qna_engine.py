8e7e1d7182bc4db04eab4a5ce1d5182fb8aeb527
Satvik <satvik.pss@gmail.com>
2024-05-24 00:59:34 +0530

    removed level as default

--- educhain/qna_engine.py
+++ educhain/qna_engine.py
@@ -8,7 +8,7 @@ from .models import MCQList ###
 
 
 
-def generate_mcq(topic, level = "Intermediate", num=1, llm=None, response_model=None , prompt_template=None, **kwargs):
+def generate_mcq(topic, num=1, llm=None, response_model=None , prompt_template=None, **kwargs):
 
     if response_model == None:
       parser = PydanticOutputParser(pydantic_object = MCQList)
@@ -23,14 +23,13 @@ def generate_mcq(topic, level = "Intermediate", num=1, llm=None, response_model=
         provide the question, four answer options, and the correct answer.
 
         Topic: {topic}
-        Level: {level}
         """
 
     # Append the JSON format instruction line to the custom prompt template
     prompt_template += "\nThe response should be in JSON format. \n {format_instructions}"
 
     MCQ_prompt = PromptTemplate(
-        input_variables=["num", "topic", "level"],
+        input_variables=["num", "topic"],
         template=prompt_template,
         partial_variables={"format_instructions": format_instructions}
     )
@@ -44,7 +43,7 @@ def generate_mcq(topic, level = "Intermediate", num=1, llm=None, response_model=
     MCQ_chain = MCQ_prompt | llm
 
     results = MCQ_chain.invoke(
-        {"num": num, "topic": topic, "level": level, **kwargs},
+        {"num": num, "topic": topic, **kwargs},
         # return_only_outputs=True
 
     )
@@ -54,3 +53,50 @@ def generate_mcq(topic, level = "Intermediate", num=1, llm=None, response_model=
 
     return structured_output
 
+
+
+# def generate_mcq(topic, level = "Intermediate", num=1, llm=None, response_model=None , prompt_template=None, **kwargs):
+
+#     if response_model == None:
+#       parser = PydanticOutputParser(pydantic_object = MCQList)
+#       format_instructions = parser.get_format_instructions()
+#     else:
+#       parser = PydanticOutputParser(pydantic_object = response_model)
+#       format_instructions = parser.get_format_instructions()
+
+#     if prompt_template is None:
+#         prompt_template = """
+#         Generate {num} multiple-choice question (MCQ) based on the given topic and level.
+#         provide the question, four answer options, and the correct answer.
+
+#         Topic: {topic}
+#         Level: {level}
+#         """
+
+#     # Append the JSON format instruction line to the custom prompt template
+#     prompt_template += "\nThe response should be in JSON format. \n {format_instructions}"
+
+#     MCQ_prompt = PromptTemplate(
+#         input_variables=["num", "topic", "level"],
+#         template=prompt_template,
+#         partial_variables={"format_instructions": format_instructions}
+#     )
+
+#     if llm:
+#         llm = llm
+#     else:
+#         llm = ChatOpenAI(model="gpt-3.5-turbo")
+
+#     # MCQ_chain = LLMChain(llm=llm, prompt=MCQ_prompt)
+#     MCQ_chain = MCQ_prompt | llm
+
+#     results = MCQ_chain.invoke(
+#         {"num": num, "topic": topic, "level": level, **kwargs},
+#         # return_only_outputs=True
+
+#     )
+
+#     results = results.content
+#     structured_output = parser.parse(results)
+
+#     return structured_output
