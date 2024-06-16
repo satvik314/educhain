354730e26c97eca9024d096f7db37c0fb3f00909
Prathmesh Sadake <prathmeshsadake@gmail.com>
2024-06-17 00:07:38 +0530

    Update __init__.py

--- educhain/__init__.py
+++ educhain/__init__.py
@@ -1,5 +1,5 @@
 from .utils import to_csv, to_json, to_pdf
 from .qna_engine import generate_mcq
-from .content_engine import generate_lesson_plan
+from .content_engine import generate_lesson_plan, generate_question_paper
 
-__all__ = ['generate_mcq', 'to_csv', 'to_json', 'to_pdf', 'generate_lesson_plan']
+__all__ = ['generate_mcq', 'to_csv', 'to_json', 'to_pdf', 'generate_lesson_plan','generate_question_paper']
