fd1defeb7f28cb785cfe3368eb4c578baa4e69df
VedantDeshmukh2 <vedant@buildfastwithai.com>
2024-09-16 15:23:31 +0530

    Update content_engine.py

--- educhain/content_engine.py
+++ educhain/content_engine.py
@@ -1,131 +1,115 @@
+from typing import List, Optional, Any
 from langchain_openai import ChatOpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.output_parsers import PydanticOutputParser
-from .models import LessonPlan,QuestionPaper , DoubtSolverConfig, SolvedDoubt
-from typing import List, Optional ,Any 
+from models import LessonPlan, QuestionPaper, DoubtSolverConfig, SolvedDoubt
+from config import EduchainConfig
 from langchain.schema import HumanMessage, SystemMessage
 import os
 import base64
 
-# Generated Lesson Plan
-def generate_lesson_plan(topic, llm=None, response_model=None, prompt_template=None, **kwargs):
-    if response_model == None:
+class ContentEngine:
+    def __init__(self, config: EduchainConfig):
+        self.config = config
+        self.llm = self._initialize_llm()
+
+    def _initialize_llm(self) -> ChatOpenAI:
+        llm_config = self.config.llm_config
+        return ChatOpenAI(
+            model_name=llm_config.model_name,
+            openai_api_key=llm_config.api_key,  # Changed from api_key_name to api_key
+            max_tokens=llm_config.max_tokens
+        )
+
+    def generate_lesson_plan(self, topic: str, prompt_template: Optional[str] = None, **kwargs) -> LessonPlan:
         parser = PydanticOutputParser(pydantic_object=LessonPlan)
         format_instructions = parser.get_format_instructions()
-    else:
-        parser = PydanticOutputParser(pydantic_object=response_model)
-        format_instructions = parser.get_format_instructions()
 
-    if prompt_template is None:
-        prompt_template = """
-        Generate a comprehensive lesson plan for the given topic and duration.
-        Include the following details in the lesson plan:
-        - Objectives: List the learning objectives of the lesson.
-        - Introduction: Provide an engaging introduction to the lesson.
-        - Content Outline: Outline the main points or sections of the lecture content.
-        - Assessment: Describe how the students' understanding will be assessed.
-        - Conclusion: Summarize the key takeaways and provide a conclusion for the lesson.
+        if prompt_template is None:
+            prompt_template = """
+            Generate a comprehensive lesson plan for the given topic and duration.
+            Include the following details in the lesson plan:
+            - Objectives: List the learning objectives of the lesson.
+            - Introduction: Provide an engaging introduction to the lesson.
+            - Content Outline: Outline the main points or sections of the lecture content.
+            - Assessment: Describe how the students' understanding will be assessed.
+            - Conclusion: Summarize the key takeaways and provide a conclusion for the lesson.
 
-        Topic: {topic}
-        """
+            Topic: {topic}
+            """
 
-    # Append the JSON format instruction line to the custom prompt template
-    prompt_template += "\nThe response should be in JSON format. \n {format_instructions}"
+        prompt_template += "\nThe response should be in JSON format. \n {format_instructions}"
 
-    lesson_plan_prompt = PromptTemplate(
-        input_variables=["topic"],
-        template=prompt_template,
-        partial_variables={"format_instructions": format_instructions}
-    )
+        lesson_plan_prompt = PromptTemplate(
+            input_variables=["topic"],
+            template=prompt_template,
+            partial_variables={"format_instructions": format_instructions}
+        )
 
-    if llm:
-        llm = llm
-    else:
-        llm = ChatOpenAI(model="gpt-3.5-turbo")
+        lesson_plan_chain = lesson_plan_prompt | self.llm
 
-    lesson_plan_chain = lesson_plan_prompt | llm
+        results = lesson_plan_chain.invoke(
+            {"topic": topic, **kwargs},
+        )
+        results = results.content
+        structured_output = parser.parse(results)
+        return structured_output
 
-    results = lesson_plan_chain.invoke(
-        {"topic": topic, **kwargs},
-    )
-    results = results.content
-    structured_output = parser.parse(results)
-    return structured_output
-
-def generate_question_paper(
-    subject: str,
-    grade_level: int,
-    num_questions: int,
-    question_types: List[str] = ['multiple_choice'],
-    time_limit: Optional[int] = None,
-    difficulty_level: Optional[str] = None,
-    topics: Optional[List[str]] = None,
-    llm=None,
-    response_model=None,
-    prompt_template=None,
-    **kwargs
-):
-    if response_model is None:
+    def generate_question_paper(self, subject: str, grade_level: int, num_questions: int, question_types: List[str] = ['multiple_choice'], time_limit: Optional[int] = None, difficulty_level: Optional[str] = None, topics: Optional[List[str]] = None, prompt_template: Optional[str] = None, **kwargs) -> QuestionPaper:
         parser = PydanticOutputParser(pydantic_object=QuestionPaper)
         format_instructions = parser.get_format_instructions()
-    else:
-        parser = PydanticOutputParser(pydantic_object=response_model)
-        format_instructions = parser.get_format_instructions()
 
-    if prompt_template is None:
-        prompt_template = """
-        Generate a {num_questions}-question multiple-choice {subject} assessment for grade {grade_level}.
-        
-        The assessment should have a time limit of {time_limit} minutes if provided, and a difficulty level of {difficulty_level} if provided.
-        The assessment should cover the following topics if provided: {topics}
-        
-        The response should be in JSON format.
-        {format_instructions}
-        """
+        if prompt_template is None:
+            prompt_template = """
+            Generate a {num_questions}-question multiple-choice {subject} assessment for grade {grade_level}.
+            
+            The assessment should have a time limit of {time_limit} minutes if provided, and a difficulty level of {difficulty_level} if provided.
+            The assessment should cover the following topics if provided: {topics}
+            
+            The response should be in JSON format.
+            {format_instructions}
+            """
 
-    QP_prompt = PromptTemplate(
-        input_variables=["subject", "grade_level", "num_questions", "time_limit", "difficulty_level", "topics"],
-        template=prompt_template,
-        partial_variables={"format_instructions": format_instructions}
-    )
+        QP_prompt = PromptTemplate(
+            input_variables=["subject", "grade_level", "num_questions", "time_limit", "difficulty_level", "topics"],
+            template=prompt_template,
+            partial_variables={"format_instructions": format_instructions}
+        )
 
-    if llm:
-        llm = llm
-    else:
-        llm = ChatOpenAI(model="gpt-3.5-turbo")
+        QP_chain = QP_prompt | self.llm
 
-    QP_chain = QP_prompt | llm
+        results = QP_chain.invoke(
+            {
+                "subject": subject,
+                "grade_level": grade_level,
+                "num_questions": num_questions,
+                "question_types": question_types,
+                "time_limit": time_limit,
+                "difficulty_level": difficulty_level,
+                "topics": topics,
+                **kwargs
+            }
+        )
 
-    results = QP_chain.invoke(
-        {
-            "subject": subject,
-            "grade_level": grade_level,
-            "num_questions": num_questions,
-            "question_types": question_types,
-            "time_limit": time_limit,
-            "difficulty_level": difficulty_level,
-            "topics": topics,
-            **kwargs
-        }
-    )
+        structured_output = parser.parse(results.content)
+        return structured_output
 
-    structured_output = parser.parse(results.content)
-
-    return structured_output
-
-# Vision Class
 class DoubtSolver:
-    def __init__(self, config: DoubtSolverConfig = DoubtSolverConfig()):
+    def __init__(self, config: EduchainConfig, doubt_solver_config: DoubtSolverConfig = DoubtSolverConfig()):
         self.config = config
+        self.doubt_solver_config = doubt_solver_config
+        self.llm = self._initialize_llm()
 
-    def solve(self, 
-              img_path: str, 
-              prompt: str = "Explain how to solve this problem", 
-              llm: Optional[Any] = None,
-              custom_instructions: Optional[str] = None,
-              **kwargs) -> Optional[SolvedDoubt]:
-        
+    def _initialize_llm(self) -> ChatOpenAI:
+        return ChatOpenAI(
+            model_name=self.doubt_solver_config.model_name,
+            openai_api_key=os.getenv(self.doubt_solver_config.api_key_name),
+            max_tokens=self.doubt_solver_config.max_tokens,
+            temperature=0,
+        )
+
+    def solve(self, img_path: str, prompt: str = "Explain how to solve this problem", custom_instructions: Optional[str] = None, **kwargs) -> Optional[SolvedDoubt]:
         if not img_path:
             raise ValueError("Image path or URL is required")
 
@@ -154,25 +138,13 @@ class DoubtSolver:
             {"type": "image_url", "image_url": {"url": image_content}}
         ])
 
-        if llm is None:
-            llm = self._get_chat_model()
-
         try:
-            response = llm([system_message, human_message])
+            response = self.llm([system_message, human_message])
             result = parser.parse(response.content)
             return result
         except Exception as e:
             print(f"Error in solve: {type(e).__name__}: {str(e)}")
             return None
-
-    def _get_chat_model(self) -> ChatOpenAI:
-        config = self.config.gpt4
-        return ChatOpenAI(
-            model_name=config.model_name, 
-            api_key=os.getenv(config.api_key_name), 
-            max_tokens=config.max_tokens,
-            temperature=0,
-        )
 
     @staticmethod
     def _get_image_content(img_path: str) -> str:
