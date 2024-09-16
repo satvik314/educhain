0193b752e876d765d6965e5ec907b79d18b827f3
Satvik <satvik.pss@gmail.com>
2024-09-16 18:47:15 +0530

    version 0.2.18

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="educhain",
-    version="0.2.16",  # Increment this from 0.2.15 to 0.2.16
+    version="0.2.18",  # Incremented from 0.2.17 to 0.2.18
     packages=find_packages(),
     install_requires=[
         "langchain",
